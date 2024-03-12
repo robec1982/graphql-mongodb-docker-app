c564efe81cbc82c7cd974856f5836c5d5e653d48
Roberto Carlos <robec1982@gmail.com>
2024-03-12 11:55:20 -0300

    Mutations (create, update, delete) added

--- graphqlapp/data/resolvers.js
+++ graphqlapp/data/resolvers.js
@@ -16,11 +16,62 @@ export const resolvers = {
         }
     },
 
-    createProduct: ({input}) => {
-        // let id = require('crypto').randomBytes(10).toString('hex');
-        // productDatabase[id] = input;
-        // return new Product(id, input);
-    }
+    createProduct: async ({input}) => {
+        try {
+            const product = new Widgets({
+                name: input.name,
+                description: input.description,
+                price: input.price,
+                soldout: input.soldout,
+                inventory: input.inventory,
+                stores: input.stores,
+            });
+            
+            product.id = product._id;
+
+            console.log(`Creating a new product ${product}.`);            
+            let response = await product.save();
+            if (response) {
+                return product;
+            }
+            else throw Error(JSON.stringify({ code: 400, message: `Error while creating the product ${product}.` }));
+        }
+        catch (error) {
+            console.error(`An error occurred while adding a new product: ${error}`);
+            return error;
+        }
+    },
+    
+    updateProduct: async ({input}) => {
+        try {
+            console.log(`Updating the product ${input.id}.`);            
+            let product = await Widgets.findOneAndUpdate({ _id: input.id }, input, { new: true });
+            if (product) {
+                return product;
+            }
+            else throw Error(JSON.stringify({ code: 400, message: `Error while updating the product with ID ${input.id}.` }));
+        }
+        catch (error) {
+            console.error(`An error occurred while adding a new product: ${error}`);
+            return error;
+        }
+    },
+
+    deleteProduct: async ({ id }) => {
+        try {
+            console.log(`Deleting the product with the ID ${id}.`);            
+            let product = await Widgets.findOneAndDelete({ _id: id });
+            if (product) {
+                console.log(`Product deleted successfully ${product}.`);
+                return product;
+            }
+            else throw Error(JSON.stringify({ code: 400, message: `Error while deleting the product with ID ${id}.` }));
+        }
+        catch (error) {
+            console.error(`An error occurred while adding a new product: ${error}`);
+            return error;
+        }
+    },
 };
 
 export default resolvers;
